e79cf2263fe5f1cd29be9768842282af6e0758d1
Jochem Janssens <janssens.jochem@gmail.com>
2020-11-23 09:02:20 +0100

    feat: add BLE provisioning for Android

--- android/src/main/java/com/reactnativeespidfprovisioning/EspIdfProvisioningModule.kt
+++ android/src/main/java/com/reactnativeespidfprovisioning/EspIdfProvisioningModule.kt
@@ -3,29 +3,123 @@ package com.reactnativeespidfprovisioning
 import android.Manifest
 import android.content.pm.PackageManager
 import android.widget.Toast
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanResult
 import androidx.core.app.ActivityCompat
+import android.util.Log
+
 import com.espressif.provisioning.ESPConstants
 import com.espressif.provisioning.ESPDevice
 import com.espressif.provisioning.ESPProvisionManager
 import com.espressif.provisioning.WiFiAccessPoint
 import com.espressif.provisioning.listeners.ProvisionListener
 import com.espressif.provisioning.listeners.WiFiScanListener
+import com.espressif.provisioning.listeners.BleScanListener
+import com.espressif.provisioning.DeviceConnectionEvent
+
+import com.facebook.react.modules.core.DeviceEventManagerModule
 import com.facebook.react.bridge.*
 import java.lang.Exception
-
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
 
 class EspIdfProvisioningModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    val foundBLEDevices = HashMap<String, BluetoothDevice>();
+
+    init {
+      EventBus.getDefault().register(this);
+    }
 
     override fun getName(): String {
         return "EspIdfProvisioning"
     }
 
-    // Example method
-    // See https://facebook.github.io/react-native/docs/native-modules-android
+    // Searches for BLE devices with a name starting with the given prefix.
+    // The prefix must match the string in '/main/app_main.c'
+    // Resolves to an array of BLE devices
     @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
+    fun getBleDevices(prefix: String, promise: Promise) {
+      Log.e("Jooki", "getBleDevices")
 
-      promise.resolve(a * b)
+      if (ActivityCompat.checkSelfPermission(reactApplicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        promise.reject("Location Permission denied")
+        return;
+      }
+
+      // Search for BLE devices
+      ESPProvisionManager.getInstance(reactApplicationContext).searchBleEspDevices(prefix, object : BleScanListener {
+        override fun scanStartFailed() {
+          promise.reject("Scan start failed")
+        }
+
+        override fun onPeripheralFound(device: BluetoothDevice, scanResult: ScanResult) {
+          foundBLEDevices[device.address] = device;
+        }
+
+        override fun scanCompleted() {
+          val result = WritableNativeArray();
+          foundBLEDevices.keys.forEach { result.pushString(it) }
+
+          // Return found BLE devices
+          promise.resolve(result);
+        }
+
+        override fun onFailure(p0: Exception?) {
+          promise.reject(p0.toString())
+        }
+      });
+    }
+
+    // Send event to JS
+    fun sendEvent(name: String, value: String) {
+      reactApplicationContext
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+            .emit(name, value);
+    }
+
+    // Subscribe on a DeviceConnectionEvent to get more info about the device connection.
+    @Subscribe
+    fun onConnectionEvent(event: DeviceConnectionEvent) {
+      Log.e("Jooki", "DeviceConnectionEvent")
+
+      when (event.getEventType()) {
+        ESPConstants.EVENT_DEVICE_CONNECTED -> sendEvent("DeviceConnectionEvent", "EVENT_DEVICE_CONNECTED")
+        ESPConstants.EVENT_DEVICE_DISCONNECTED -> sendEvent("DeviceConnectionEvent", "EVENT_DEVICE_DISCONNECTED")
+        ESPConstants.EVENT_DEVICE_CONNECTION_FAILED -> sendEvent("DeviceConnectionEvent", "EVENT_DEVICE_CONNECTION_FAILED")
+      }
+    }
+
+    // Connects to a BLE device
+    // We need the Service UUID from the config.service_uuid in app_prov.c
+    // We need the proof of possestion (pop) specified in '/main/app_main.c'
+    // The deviceAddress is the address we got from the "searchBleEspDevices" function
+    // Resolves when connected to device
+    @ReactMethod
+    fun connectBleDevice(deviceAddress: String, deviceProofOfPossession: String, promise: Promise) {
+      Log.e("Jooki", "connectBleDevice")
+      if (ActivityCompat.checkSelfPermission(reactApplicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        promise.reject("Location Permission denied");
+        return;
+      }
+
+      // SECURITY_0 is plain text communication.
+      // SECURITY_1 is encrypted.
+      // This must match 'wifi_prov_security_t security' in app_main.c
+      val esp : ESPDevice = ESPProvisionManager.getInstance(reactApplicationContext).createESPDevice(ESPConstants.TransportType.TRANSPORT_BLE, ESPConstants.SecurityType.SECURITY_0);
+      esp.proofOfPossession = deviceProofOfPossession
+
+      // TODO(wdm) What is the name of this service, and therefore what should this const be called?
+      val SERVICE_UUID = "021a9004-0382-4aea-bff4-6b3f1c5adfb4"; // See config.service_uuid in app_prov.c
+
+      val device = foundBLEDevices[deviceAddress]
+      if (device == null) {
+        promise.reject("Invalid address $deviceAddress")
+        return;
+      }
+
+      esp.connectBLEDevice(device, SERVICE_UUID);
+
+      promise.resolve(esp.deviceName);
     }
 
     @ReactMethod
@@ -42,22 +136,27 @@ class EspIdfProvisioningModule(reactContext: ReactApplicationContext) : ReactCon
     }
 
     @ReactMethod
-    fun scanWifiList(callback: Callback) {
+    fun scanWifiList(promise: Promise) {
       val device = ESPProvisionManager.getInstance(reactApplicationContext).espDevice
-      if(device != null) {
-//        val listener: WiFiScanListener = WiFiScanListener()
-        device.scanNetworks(object: WiFiScanListener {
-          override fun onWifiListReceived(wifiList: java.util.ArrayList<WiFiAccessPoint>?) {
-            callback.invoke(wifiList)
-          }
 
-          override fun onWiFiScanFailed(p0: java.lang.Exception?) {
-            Toast.makeText(reactApplicationContext, "Failed to get Wi-Fi scan list", Toast.LENGTH_LONG).show();
-          }
-        })
-      } else {
-        Toast.makeText(reactApplicationContext, "No device found", Toast.LENGTH_LONG).show();
+      if(device == null) {
+        promise.reject("No device found")
+        return;
       }
+
+      device.scanNetworks(object: WiFiScanListener {
+        override fun onWifiListReceived(wifiList: java.util.ArrayList<WiFiAccessPoint>?) {
+          val result = WritableNativeArray();
+          wifiList?.forEach {
+            result.pushString(it.wifiName)
+          }
+          promise.resolve(result)
+        }
+
+        override fun onWiFiScanFailed(p0: java.lang.Exception?) {
+          promise.reject("Failed to get Wi-Fi scan list")
+        }
+      })
     }
 
     @ReactMethod
